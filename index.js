2b7c686c65de0a309a5f3941cb27fb0814be8c84
Toon Ketels <toon@puresign.be>
2011-12-10 18:31:08 +0100

    Created router with requestHandler. RequestHandler is not yet doing something.

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 var server = require("./server");
 var router = require( "./router" );
+var requestHandlers = require( "./requestHandlers" );
 
-server.start( router.route );
+// List of urls we want to handle/map
+// We pass the entire list to the server,
+// which will pass it to the rooter
+// => all action happens in the server
+var handle = {};
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+
+server.start( router.route, handle );
 
