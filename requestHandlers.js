3413972abce9c8dbc78c05ea3b805bc3ee05bfaa
Toon Ketels <toon@puresign.be>
2011-12-11 09:19:16 +0100

    Played with blocking requestHandler some more. Dont get it to work. @todo Look into it later.

--- requestHandlers.js
+++ requestHandlers.js
@@ -5,6 +5,21 @@
 
 function start() {
   console.log( "Request handler 'start' was called." );
+  
+  // Some blocking functionality.
+  // This keeps the requesthandler buzy and form responding
+  // till 10 seconds passed.
+  // In real life, this might be some computation
+  function sleep(milliSeconds) {
+    var startTime = new Date().getTime();
+    while (new Date().getTime < startTime + milliSeconds);
+    console.log('sleeping');
+  }
+  
+  console.log('About to call sleep');
+  sleep(100000);
+  console.log('Actually called sleep');
+  console.log('About to return text');
   return "Hello world";
 }
 
